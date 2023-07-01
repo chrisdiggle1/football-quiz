496e5f5fb84ff0d8389b38e9c3dee7991024cd42
chrisdiggle1 <>
2023-07-01 10:46:21 +0000

    add function to display the main quiz area once the start quiz button is pressed

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,15 @@
+//Constants for DOM elements
 const startButton = document.getElementById('start-btn');
+const questionContainer = document.getElementById('question-container');
 
-
+//Opens the main quiz game.
 startButton.addEventListener('click', startQuiz);
 
 function startQuiz() {
-    console.log('Started')
+    // Hide the home area
+    const homeArea = document.getElementById('quiz-container');
+    homeArea.style.display = 'none';
+
+    // Show the question container
+    questionContainer.classList.remove('hidden');
 }
