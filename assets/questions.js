9f09980653a625152b0fc600986468e0daa34bd3
chrisdiggle1 <>
2023-07-01 14:30:09 +0000

    create questions.js file, link from index.html. Add questions for the game

---
+++ assets/questions.js
@@ -0,0 +1,95 @@
+// An array containing 10 questions and answers, including the correct answer for the quiz game.
+
+const questions = [
+    {
+        question: "Which of the following is not a football team in London?",
+        answers: [
+            { text: "Chelsea FC", correct: false},
+            { text: "Tottenham Hotspur FC", correct: false},
+            { text: "Everton FC", correct: true},
+            { text: "Arsenal FC", correct: false},
+            
+        ]
+    },
+    {
+        question: "How many countries have won the World Cup",
+        answers: [
+            { text: "8", correct: true},
+            { text: "10", correct: false},
+            { text: "12", correct: false},
+            { text: "14", correct: false},
+        ]
+    },
+    {
+        question: "When was the football league in England founded?",
+        answers: [
+            { text: "1866", correct: false},
+            { text: "1888", correct: true},
+            { text: "1911", correct: false},
+            { text: "1933", correct: false},
+        ]
+    },
+    {
+        question: "How many people watched the final between France and Croatia at the FIFA World Cup of 2018?",
+        answers: [
+            { text: "1.12 million", correct: false},
+            { text: "12 million", correct: false},
+            { text: "112 million", correct: false},
+            { text: "1.12 billion", correct: true},
+        ]    
+    },
+    {
+        question: "How many international goals did Mia Hamm score in her career?",
+        answers: [
+            { text: "38", correct: false},
+            { text: "108", correct: false},
+            { text: "158", correct: true},
+            { text: "248", correct: false},
+        ]    
+    },
+    {
+        question: "When was football first introduced in the Olympics?",
+        answers: [
+            { text: "1888", correct: false},
+            { text: "1900", correct: true},
+            { text: "1912", correct: false},
+            { text: "1924", correct: false},
+        ]    
+    },
+    {
+        question: "Which footballer holds the record for the highest number of assists in the Premier League?",
+        answers: [
+            { text: "Cesc Fabregas", correct: false},
+            { text: "Ryan Giggs", correct: true},
+            { text: "Frank Lampard", correct: false},
+            { text: "Paul Scholes", correct: false},
+        ]    
+    },
+    {
+        question: "What is the standard size of a football pitch?",
+        answers: [
+            { text: "125 m x 85 m", correct: true},
+            { text: "155 m x 95 m", correct: false},
+            { text: "175 m x 105 m", correct: false},
+            { text: "1195 m x 110 m", correct: false},
+        ]    
+    },
+    {
+        question: "Who won the UEFA Europ Championship in 2020?",
+        answers: [
+            { text: "France", correct: false},
+            { text: "Italy", correct: true},
+            { text: "England", correct: false},
+            { text: "Germany", correct: false},
+        ]
+    },
+    {
+        question: "How many ballons d’or did Lionel Messi receive",
+        answers: [
+            { text: "4", correct: false},
+            { text: "6", correct: false},
+            { text: "8", correct: true},
+            { text: "10", correct: false},
+        ]
+    }
+];
