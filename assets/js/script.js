8bff8237d7208e3c83bf0e3b5087b3bcf230bc5e
chrisdiggle1 <>
2023-07-05 18:04:46 +0000

    add comments explaining the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Certain aspects of the code were guided and inspired from the Love Maths walkthrough on the Code Institutes LMS.
+// Elements of code were taken and adapted from - Web Dev Simplified - https://www.youtube.com/watch?v=riDzcEQbX6k
+// The questions for the quiz were taken from - https://www.quiztriviagames.com/football-quiz/
+
 //Wait for the DOM to finish loading before running the game.
 //Constants for DOM elements.
 document.addEventListener('DOMContentLoaded', function () {
@@ -18,9 +22,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     /**
      * This function initiates the quiz, hiding the home page and displaying the questions.
-     * The function sets the current question index to 0 and the the score to 0.
-     * The function the inner HTML of the 'Next Question' button.
-     * The function the the showQuestion functionto display current question.
+     * It resets the current question index and score, and updates the right and wrong counter.
+     * It changes the innerHTML of the next button element to display the first question.
+     * Calls the show question function to display the first question.
      */
     function startQuiz() {
         const homeArea = document.querySelector('.home-area');
@@ -71,14 +75,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-
+    /**
+     * This function determines whether the selected answer is correct based on the 'correct' attribute.
+     * It increments the score and updates the answer is correct, and increments the count of incorrect answers.
+     * Disables all the answer buttons and displays the 'Next Question button once an answer has been selected. 
+     */
     function selectAnswer(event) {
         const selectedBtn = event.target;
         const isCorrect = selectedBtn.getAttribute('data-correct') === 'true';
         if (isCorrect) {
             selectedBtn.classList.add('correct');
             score++;
-            incrementScore(); 
+            incrementScore();
         } else {
             selectedBtn.classList.add('incorrect');
             incrementWrongAnswer();
@@ -92,6 +100,10 @@ document.addEventListener('DOMContentLoaded', function () {
         nextButton.style.display = 'block';
     }
 
+    /**
+     * The function displays the final score and provides the option to play again.
+     * The quiz is reset if the user selects to play again.
+     */
     function showScore() {
         resetState();
         questionElement.innerHTML = `You scored ${score} goals from ${questions.length} shots!`;
@@ -99,28 +111,45 @@ document.addEventListener('DOMContentLoaded', function () {
         nextButton.style.display = 'block'
     }
 
+    /**
+     * This function handles the action when the 'Next Question' button is clicked.
+     * It increments the current question index and checks if any more questions are remaining.
+     * The next question will be displayed if there are any, if not, it will show the call the show score function.
+     */
     function clickNextButton() {
         currentQuestionIndex++;
-        if(currentQuestionIndex < questions.length) {
+        if (currentQuestionIndex < questions.length) {
             showQuestion();
         } else {
             showScore();
         }
     }
 
+    /**
+     * Event listener for for when the'Next Question' button is clicked.
+     * checks if there are any more questions left to answer,
+     * if there are, it calls the click next button function,
+     * if there are no more questions, the final score and option to play again is displayed.
+     */
     nextButton.addEventListener('click', () => {
-        if(currentQuestionIndex < questions.length) {
+        if (currentQuestionIndex < questions.length) {
             clickNextButton();
-        }else{
+        } else {
             startQuiz();
         }
     })
 
+    /**
+     * This function obtains the the current score from the DOM and increments it by 1.
+     */
     function incrementScore() {
         let oldScore = parseInt(document.getElementById('right-counter').innerText);
         document.getElementById('right-counter').innerText = ++oldScore;
     }
 
+    /**
+     * This function obtains the current tally of incorrect answers from the DOM and increments by 1.
+     */
     function incrementWrongAnswer() {
         let oldScore = parseInt(document.getElementById('wrong-counter').innerText);
         document.getElementById('wrong-counter').innerText = ++oldScore;
