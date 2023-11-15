cee6e71c822038ecb3400081ea500ac0233a4413
chrisdiggle1 <>
2023-11-15 10:48:26 +0000

    Add 10 questions for easy difficulty

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,95 +1,103 @@
-// An array containing 10 questions and answers, including the correct answer for the quiz game.
+// An array that holds all the questions and possible answers, including the corrent answer.
 
-const questions = [
-    {
-        question: "Which of the following is not a football team in London?",
-        answers: [
-            { text: "Chelsea FC", correct: false },
-            { text: "Tottenham Hotspur FC", correct: false },
-            { text: "Everton FC", correct: true },
-            { text: "Arsenal FC", correct: false },
+const questions = {
+    easy: [
+        {
+            question: "Which of the following is not a football team in London?",
+            answers: [
+                { text: "Chelsea FC", correct: false },
+                { text: "Tottenham Hotspur FC", correct: false },
+                { text: "Everton FC", correct: true },
+                { text: "Arsenal FC", correct: false },
+            ]
+        },
+        {
+            question: "If there is a clean sheet, how many goals are scored?",
+            answers: [
+                { text: "0", correct: true },
+                { text: "2", correct: false },
+                { text: "4", correct: false },
+                { text: "6", correct: false },
+            ]
+        },
+        {
+            question: "Who won the UEFA European Championship in 2020?",
+            answers: [
+                { text: "France", correct: false },
+                { text: "Italy", correct: true },
+                { text: "England", correct: false },
+                { text: "Germany", correct: false },
+            ]
+        },
 
-        ]
-    },
-    {
-        question: "How many countries have won the World Cup?",
-        answers: [
-            { text: "8", correct: true },
-            { text: "10", correct: false },
-            { text: "12", correct: false },
-            { text: "14", correct: false },
-        ]
-    },
-    {
-        question: "When was the football league in England founded?",
-        answers: [
-            { text: "1866", correct: false },
-            { text: "1888", correct: true },
-            { text: "1911", correct: false },
-            { text: "1933", correct: false },
-        ]
-    },
-    {
-        question: "How many people watched the final between France and Croatia at the FIFA World Cup of 2018?",
-        answers: [
-            { text: "1.12 million", correct: false },
-            { text: "12 million", correct: false },
-            { text: "112 million", correct: false },
-            { text: "1.12 billion", correct: true },
-        ]
-    },
-    {
-        question: "How many international goals did Mia Hamm score in her career?",
-        answers: [
-            { text: "38", correct: false },
-            { text: "108", correct: false },
-            { text: "158", correct: true },
-            { text: "248", correct: false },
-        ]
-    },
-    {
-        question: "When was football first introduced in the Olympics?",
-        answers: [
-            { text: "1888", correct: false },
-            { text: "1900", correct: true },
-            { text: "1912", correct: false },
-            { text: "1924", correct: false },
-        ]
-    },
-    {
-        question: "Which footballer holds the record for the highest number of assists in the Premier League?",
-        answers: [
-            { text: "Cesc Fabregas", correct: false },
-            { text: "Ryan Giggs", correct: true },
-            { text: "Frank Lampard", correct: false },
-            { text: "Paul Scholes", correct: false },
-        ]
-    },
-    {
-        question: "What is the standard size of a football pitch?",
-        answers: [
-            { text: "125 m x 85 m", correct: true },
-            { text: "155 m x 95 m", correct: false },
-            { text: "175 m x 105 m", correct: false },
-            { text: "1195 m x 110 m", correct: false },
-        ]
-    },
-    {
-        question: "Who won the UEFA European Championship in 2020?",
-        answers: [
-            { text: "France", correct: false },
-            { text: "Italy", correct: true },
-            { text: "England", correct: false },
-            { text: "Germany", correct: false },
-        ]
-    },
-    {
-        question: "How many ballons d’or did Lionel Messi receive?",
-        answers: [
-            { text: "4", correct: false },
-            { text: "6", correct: false },
-            { text: "8", correct: true },
-            { text: "10", correct: false },
-        ]
-    }
-];
+        {
+            question: "Which football club did Lionel Messi join in 2021?",
+            answers: [
+                { text: "Man City", correct: false },
+                { text: "PSG", correct: true },
+                { text: "Barcelona", correct: false },
+                { text: "Liverpool", correct: false },
+            ]
+        },
+
+        {
+            question: "What is the maximum number of players a football team can have on the field at any time?",
+            answers: [
+                { text: "9", correct: false },
+                { text: "11", correct: true },
+                { text: "10", correct: false },
+                { text: "12", correct: false },
+            ]
+        },
+
+        {
+            question: "Which country is home to the famous football club Barcelona?",
+            answers: [
+                { text: "Italy", correct: false },
+                { text: "Spain", correct: true },
+                { text: "Portugal", correct: false },
+                { text: "France", correct: false },
+            ]
+        },
+
+        {
+            question: "How long is a standard professional football match, excluding extra time?",
+            answers: [
+                { text: "60 minutes", correct: false },
+                { text: "70 minutes", correct: false },
+                { text: "80 minutes", correct: false },
+                { text: "90 minutes", correct: true },
+            ]
+        },
+
+        {
+            question: "What color card does a referee show for a direct dismissal from a football game?",
+            answers: [
+                { text: "Yellow", correct: false },
+                { text: "Green", correct: false },
+                { text: "Red", correct: true },
+                { text: "Blue", correct: false },
+            ]
+        },
+
+        {
+            question: "Who won the FIFA World Cup in 2018?",
+            answers: [
+                { text: "France", correct: true },
+                { text: "Brazil", correct: false },
+                { text: "Germany", correct: false },
+                { text: "Argentina", correct: false },
+            ]
+        },
+
+        {
+            question: "In which year was the Premier League founded?",
+            answers: [
+                { text: "1988", correct: false },
+                { text: "1992", correct: true },
+                { text: "1996", correct: false },
+                { text: "2000", correct: false },
+            ]
+        },
+    ]
+};
