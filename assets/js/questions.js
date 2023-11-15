dac4ff55aeeb963ff02e7e037abed52efce1ce97
chrisdiggle1 <>
2023-11-15 11:55:58 +0000

    Add and test medium difficulty questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -99,5 +99,106 @@ const questions = {
                 { text: "2000", correct: false },
             ]
         },
+    ],
+    medium: [
+        {
+            question: "Which country has won the most FIFA World Cup titles?",
+            answers: [
+                { text: "Brazil", correct: true },
+                { text: "Germany", correct: false },
+                { text: "Italy", correct: false },
+                { text: "Argentina", correct: false },
+            ]
+        },
+
+        {
+            question: "In which year did Lionel Messi win his first Ballon d'Or?",
+            answers: [
+                { text: "2007", correct: false },
+                { text: "2009", correct: true },
+                { text: "2010", correct: false },
+                { text: "2012", correct: false },
+            ]
+        },
+
+        {
+            question: "Which team did Manchester United defeat in the 1999 UEFA Champions League final?",
+            answers: [
+                { text: "Bayern Munich", correct: true },
+                { text: "Juventus", correct: false },
+                { text: "Barcelona", correct: false },
+                { text: "Real Madrid", correct: false },
+            ]
+        },
+
+        {
+            question: "Which player scored the 'Hand of God' goal in the World Cup?",
+            answers: [
+                { text: "Pelé", correct: false },
+                { text: "Diego Maradona", correct: true },
+                { text: "Lionel Messi", correct: false },
+                { text: "Cristiano Ronaldo", correct: false },
+            ]
+        },
+
+        {
+            question: "Who was the first player to score in three different UEFA Champions League finals?",
+            answers: [
+                { text: "Cristiano Ronaldo", correct: false },
+                { text: "Lionel Messi", correct: false },
+                { text: "Raúl", correct: false },
+                { text: "Samuel Eto'o", correct: true },
+            ]
+        },
+
+        {
+            question: "Which country hosted the 2006 FIFA World Cup?",
+            answers: [
+                { text: "France", correct: false },
+                { text: "Germany", correct: true },
+                { text: "South Africa", correct: false },
+                { text: "Brazil", correct: false },
+            ]
+        },
+
+        {
+            question: "What is the record number of goals scored by a single player in a Premier League season?",
+            answers: [
+                { text: "31", correct: false },
+                { text: "32", correct: false },
+                { text: "34", correct: false },
+                { text: "36", correct: true },
+            ]
+        },
+
+        {
+            question: "Which club won the first Premier League title in 1992-1993?",
+            answers: [
+                { text: "Manchester United", correct: true },
+                { text: "Arsenal", correct: false },
+                { text: "Chelsea", correct: false },
+                { text: "Liverpool", correct: false },
+            ]
+        },
+
+        {
+            question: "What year was the UEFA Champions League rebranded from the European Cup?",
+            answers: [
+                { text: "1980", correct: false },
+                { text: "1985", correct: false },
+                { text: "1992", correct: true },
+                { text: "1995", correct: false },
+            ]
+        },
+
+        {
+            question: "Which player holds the record for the most goals in a calendar year?",
+            answers: [
+                { text: "Lionel Messi", correct: true },
+                { text: "Cristiano Ronaldo", correct: false },
+                { text: "Pelé", correct: false },
+                { text: "Gerd Müller", correct: false },
+            ]
+        },
     ]
 };
